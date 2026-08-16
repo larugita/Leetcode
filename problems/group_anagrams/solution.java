633e095e9bcb5eeb34e575c199b800dd09e6a38c
larugíta <nassimwarnerr@gmail.com>
2026-08-16 14:46:16 +0000

    Sync LeetCode submission - Group Anagrams (java)

--- problems/group_anagrams/solution.java
+++ problems/group_anagrams/solution.java
@@ -1,17 +1,29 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap <String, List<String>> map = new HashMap<>();
 
-        for(String s : strs){
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String word = new String(arr);
+        HashMap<List<Integer>, List<String>> map = new HashMap<>();
 
-            if(!map.containsKey(word)){
-                map.put(word, new ArrayList<>());
+        for (String s : strs) {
+
+            int[] count = new int[26];
+
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
             }
-            map.get(word).add(s);
+
+            List<Integer> key = new ArrayList<>();
+
+            for (int num : count) {     
+                key.add(num);
+            }
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(s);
         }
+
         return new ArrayList<>(map.values());
     }
 }
