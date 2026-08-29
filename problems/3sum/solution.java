ce6a494f035fd36e63a7f54d842cbbda207e202e
larugíta <nassimwarnerr@gmail.com>
2026-08-29 14:25:33 +0000

    Sync LeetCode submission - 3Sum (java)

--- problems/3sum/solution.java
+++ problems/3sum/solution.java
@@ -1,45 +1,49 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
+        Arrays.sort(nums);
+
         List<List<Integer>> result = new ArrayList<>();
 
-        Arrays.sort(nums);
+        //[-4,-4,-1,-1,0,0,1,1,2,2]
 
         for(int i = 0; i < nums.length; i++){
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
-            int sum = 0 - nums[i];
+            int target = 0-nums[i];
+
             int j = i+1;
             int k = nums.length-1;
 
             while(j < k){
-                if(j < k && nums[j] + nums[k] < sum){
-                    j++;
-                }
-                else if(j < k && nums[j] + nums[k] > sum){
+                if(nums[j] + nums[k] > target){
+                    while(j < k && nums[j] + nums[k] > target){
                     k--;
+                    }
                 }
-                else{
-                    List<Integer> arr = new ArrayList<>();
-                    arr.add(nums[i]);
-                    arr.add(nums[j]);
-                    arr.add(nums[k]);
-                    result.add(arr);
-
-                    while(j < k && nums[j] == nums[j+1]){
+                else if(nums[j] + nums[k] < target){
+                    while(j < k && nums[j] + nums[k] < target){
                         j++;
                     }
-                    while(j < k && nums[k] == nums[k-1]){
-                        k--;
-                    }
+                }
+                else{
+                   List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[j]);
+                    triplet.add(nums[k]); 
+                    result.add(triplet);
                     j++;
                     k--;
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
                 }
             }
         }
-
         return result;
-        
     }
 }
