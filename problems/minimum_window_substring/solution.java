7120a884167ba62e44e72993ebd8965fd4afe9cf
larugíta <nassimwarnerr@gmail.com>
2026-08-31 17:26:30 +0000

    Sync LeetCode submission - Minimum Window Substring (java)

--- problems/minimum_window_substring/solution.java
+++ problems/minimum_window_substring/solution.java
@@ -1,9 +1,7 @@
 class Solution {
     public String minWindow(String s, String t) {
 
-        if(t.length() > s.length() || t.equals("")){
-            return "";
-        }
+        if(t.length() > s.length() || t.length() == 0) return "";
 
         HashMap<Character, Integer> need = new HashMap<>();
         HashMap<Character, Integer> window = new HashMap<>();
@@ -12,37 +10,41 @@ class Solution {
             need.put(c, need.getOrDefault(c, 0) + 1);
         }
 
-        int have = 0, needCount = need.size();
-        int resLeft = 0, resLength = Integer.MAX_VALUE;
+        int needCount = need.size();
+        int haveCount = 0;
         int left = 0;
-        
+        int windowSize = Integer.MAX_VALUE;
+        int resLeft = 0;
         for(int right = 0; right < s.length(); right++){
             char c = s.charAt(right);
             window.put(c, window.getOrDefault(c, 0) + 1);
-            
-            if(need.containsKey(c) && window.get(c).equals(need.get(c))){
-                have++;
-            }
 
-            while(have == needCount){
-                if(right - left + 1 < resLength){
-                    resLength = right - left + 1;
+            if(need.containsKey(c)){
+                if(need.get(c).equals(window.get(c))){
+                    haveCount++;
+                }
+            }
+            
+            while(haveCount == needCount){
+                int size = right - left + 1;
+                if(size < windowSize){
+                    windowSize = size;
                     resLeft = left;
                 }
 
                 window.put(s.charAt(left), window.get(s.charAt(left)) - 1);
 
                 if(need.containsKey(s.charAt(left)) && window.get(s.charAt(left)) < need.get(s.charAt(left))){
-                    have--;
+                    haveCount--;
                 }
 
                 left++;
-
             }
         }
-        if(resLength == Integer.MAX_VALUE){
-            return "";
-        }
-        return s.substring(resLeft, resLeft + resLength);
+
+        if(windowSize == Integer.MAX_VALUE) return "";
+
+        return s.substring(resLeft, resLeft + windowSize);
+        
     }
 }
